93c62121f855e9b511c602fe0fc8684c0761e471
thlinde <46750817+thlinde@users.noreply.github.com>
2021-03-09 20:57:55 +0100

    install wave ui, roboto, primevue, line-awesome

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
-  <app-navigation />
-  <router-view />
+  <w-app>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <app-navigation />
+    <router-view />
+  </w-app>
 </template>
 
 <script lang="ts">
@@ -21,11 +23,10 @@ export default defineComponent({
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Roboto", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,7 +1,24 @@
-import {createApp} from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 import router from '/@/router';
 
-createApp(App)
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import 'line-awesome/dist/line-awesome/css/line-awesome.min.css';
+import '@fontsource/roboto';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import WaveUI from 'wave-ui';
+import 'wave-ui/dist/wave-ui.css';
+
+const app = createApp(App);
+
+new WaveUI(app, {});
+
+app
   .use(router)
+  .use(PrimeVue)
   .mount('#app');
